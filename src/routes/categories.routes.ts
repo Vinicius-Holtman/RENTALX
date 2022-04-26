b43c8ec8562469b70e031cd2db7652a5b06068cd
Vinicius <viniholtman28@gmail.com>
2022-04-26 15:39:28 -0300

    Created verify if category alredy exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,6 +7,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/categories", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: "Category Already Exists! 😩"})
+  }
+
   categoriesRepository.create({ name, description })
 
   return res.status(201).send();
