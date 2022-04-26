8bb866590c906f949f7077e91c29f99de39c219c
Vinicius <viniholtman28@gmail.com>
2022-04-26 16:44:53 -0300

    Implement Liskov Substituion Principle

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,13 +1,8 @@
 import { Category } from "../model/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
 
-// DTO => Data Transfer Object
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
-
-class CategoriesRepository {
-  private categories: Category[] = [];
+class CategoriesRepository implements ICategoriesRepository {
+  private categories: Category[];
 
   constructor() {
     this.categories = [];
