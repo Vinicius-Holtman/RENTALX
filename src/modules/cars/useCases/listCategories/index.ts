8d381532759a8caedbd33c007c5e5f800917ca5e
Vinicius <viniholtman28@gmail.com>
2022-04-27 10:27:56 -0300

    Implemented Singleton Pattern

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -3,7 +3,7 @@ import { ListCategoryController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 
-const categoriesRepository = new CategoriesRepository()
+const categoriesRepository = CategoriesRepository.getInstance()
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository)
 const listCategoriesController = new ListCategoryController(listCategoriesUseCase)
 
