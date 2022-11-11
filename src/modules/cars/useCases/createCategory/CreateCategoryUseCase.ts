8ab84dea5a6639982a2f750d3acf93ab3a59926f
Vinicius <viniholtman28@gmail.com>
2022-11-11 15:16:53 -0300

    Initialized tsyringe to instance singleton

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,4 +1,5 @@
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import { inject, injectable } from "tsyringe"
 
 interface IRequest {
   name: string;
@@ -10,8 +11,12 @@ interface IRequest {
 * [x] - Definir o retorno de erro
 * [x] - Acessar o repositorio
 */
+@injectable()
 class CreateCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) {}
   
   async execute({ name, description }: IRequest): Promise<void> {
     const categoryAlreadyExists = await this.categoriesRepository.findByName(name);
