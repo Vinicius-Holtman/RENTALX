9ee41e1d44001bbb58d727c75d11660de3c8fb6a
Vinicius <viniholtman28@gmail.com>
2022-11-14 01:54:42 -0300

    Implemented class do check errros

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken"
+import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
 
 interface IPayload {
@@ -22,7 +23,7 @@ export async function ensureAuthenticated(req: Request, res: Response, next: Nex
     const user = await usersRepository.findById(user_id)
 
     if (!user) {
-      throw new Error("User does not exists!")
+      throw new AppError("User does not exists!")
     }
 
     next()
