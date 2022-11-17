59a8f2af8996fcfec80076e026fe1f3b3f9afcf1
Vinicius <viniholtman28@gmail.com>
2022-11-17 12:20:45 -0300

    Created entity car

---
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -0,0 +1,17 @@
+import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { Car } from "../entities/Car";
+
+
+
+
+
+class CarsRepository implements ICarsRepository {
+  create(data: ICreateCarDTO): Promise<Car> {
+    throw new Error("Method not implemented.");
+  }
+  findByLicensePlate(license_plate: string): Promise<Car> {
+    throw new Error("Method not implemented.");
+  }
+  
+}
