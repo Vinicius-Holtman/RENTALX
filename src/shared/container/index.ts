290484126dd7707d53cfac9f78b441d3fae8fcad
Vinicius <viniholtman28@gmail.com>
2022-11-17 12:31:00 -0300

    Created controller

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -7,6 +7,8 @@ import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificat
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 
 // Pass inteface ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
@@ -22,4 +24,9 @@ container.registerSingleton<ISpecificationRepository>(
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
+);
+
+container.registerSingleton<ICarsRepository>(
+  "CarsRepository",
+  CarsRepository
 );
