b3351a57ea98fbbd00ecd0e6da60d6ec10fd1d41
Vinicius <viniholtman28@gmail.com>
2022-11-17 15:49:32 -0300

    Created route to list cars

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -1,7 +1,6 @@
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
-
-
+import { inject, injectable } from "tsyringe";
 
 interface IRequest {
   category_id?: string;
@@ -9,8 +8,10 @@ interface IRequest {
   name?: string;
 }
 
+@injectable()
 class ListAvailableCarsUseCase {
   constructor(
+    @inject("CarsRepository")
     private carsRepository: ICarsRepository
   ) {}
 
