d3223e513ed19a926beb6510a77ffe66323c5437
Vinicius <viniholtman28@gmail.com>
2022-11-17 14:30:18 -0300

    Created middleware to check if user is admin

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import multer from 'multer';
 import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
 import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
 import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 const categoriesRoutes = Router();
 
@@ -15,10 +17,10 @@ const createCategoryController = new CreateCategoryController()
 const importCategoryController = new ImportCategoryController()
 const listCategoriesController = new ListCategoriesController()
 
-categoriesRoutes.post("/categories", createCategoryController.handle)
+categoriesRoutes.post("/categories", ensureAuthenticated, ensureAdmin, createCategoryController.handle)
 
 categoriesRoutes.get("/categories", listCategoriesController.handle)
 
-categoriesRoutes.post("/categories/import", upload.single("file"), importCategoryController.handle)
+categoriesRoutes.post("/categories/import", ensureAuthenticated, ensureAdmin, upload.single("file"), importCategoryController.handle)
 
 export { categoriesRoutes }
--- src/shared/infra/http/routes/specification.routes.ts
+++ src/shared/infra/http/routes/specification.routes.ts
@@ -1,14 +1,16 @@
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { Router } from "express";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const createSpecificationController = new CreateSpecificationController();
 
 const specificationsRoutes = Router();
 
-specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post(
   "/specifications",
+  ensureAuthenticated,
+  ensureAdmin,
   createSpecificationController.handle
 );
 
