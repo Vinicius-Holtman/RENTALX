e760d4090a576f8636864bce0d2c206503a0a4ce
Vinicius <viniholtman28@gmail.com>
2022-11-18 11:36:36 -0300

    Created repository in memory

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,9 +1,10 @@
 import { Rental } from "../infra/typeorm/entities/Rental"
-
+import { ICreateRentalDTO } from "../dtos/ICreateRentalDTO";
 
 interface IRentalsRepository {
-  findOpenRentalByCar(card_id: string): Promise<Rental>
-  findOpenRentalByUser(user_id: string): Promise<Rental>
+  findOpenRentalByCar(card_id: string): Promise<Rental>;
+  findOpenRentalByUser(user_id: string): Promise<Rental>;
+  create(data: ICreateRentalDTO): Promise<Rental>;
 }
 
 export { IRentalsRepository }
--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -1,3 +1,4 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../IRentalsRepository";
 
@@ -5,12 +6,35 @@ class RentalRepositoryInMemory implements IRentalsRepository {
   rentals: Rental[] = [];
 
   async findOpenRentalByCar(card_id: string): Promise<Rental> {
-    return this.rentals.find(rental => rental.card_id === card_id && rental.end_date === null)
+    return this.rentals.find(
+      (rental) => rental.card_id === card_id && rental.end_date === null
+    );
   }
 
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    return this.rentals.find(rental => rental.user_id === user_id && rental.end_date === null)
+    return this.rentals.find(
+      (rental) => rental.user_id === user_id && rental.end_date === null
+    );
+  }
+
+  async create({
+    car_id,
+    user_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+
+    Object.assign(rental, {
+      car_id,
+      user_id,
+      expected_return_date,
+      start_date: new Date()
+    });
+
+    this.rentals.push(rental)
+
+    return rental;
   }
 }
 
-export { RentalRepositoryInMemory }
+export { RentalRepositoryInMemory };
