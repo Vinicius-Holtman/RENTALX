be6068ce66c9a572112840d7c946592ade354f10
Vinicius <viniholtman28@gmail.com>
2022-11-21 15:26:28 -0300

    Inject user tokens repository in tsyringe

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -14,6 +14,8 @@ import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRep
 import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
+import { IUserTokensRepository } from "@modules/accounts/repositories/IUserTokensRepository";
+import { UserTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UserTokensRepository";
 
 // Pass inteface ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
@@ -44,4 +46,9 @@ container.registerSingleton<ICarsImagesRepository>(
 container.registerSingleton<IRentalsRepository>(
   "RentalsRepository",
   RentalsRepository
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  "UserTokensRepository",
+  UserTokensRepository
 );
