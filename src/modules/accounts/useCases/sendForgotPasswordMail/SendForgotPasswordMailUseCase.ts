fbfd25858c7cfcf2cb8c37dc946fb9a45f1c9e49
Vinicius <viniholtman28@gmail.com>
2022-11-22 15:39:41 -0300

    Estilizado mensagem de envio do email

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -3,8 +3,9 @@ import { IUserTokensRepository } from "@modules/accounts/repositories/IUserToken
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { AppError } from "@shared/errors/AppError";
+import { resolve } from "path";
 import { inject, injectable } from "tsyringe";
-import { v4 as uuidV4 } from "uuid"
+import { v4 as uuidV4 } from "uuid";
 
 @injectable()
 class SendForgotPasswordMailUseCase {
@@ -20,24 +21,42 @@ class SendForgotPasswordMailUseCase {
   ) {}
 
   async execute(email: string) {
-    const user = await this.usersRepository.findByEmail(email)
+    const user = await this.usersRepository.findByEmail(email);
+
+    const templatePath = resolve(
+      __dirname,
+      "..",
+      "..",
+      "views",
+      "forgotPassword.hbs"
+    );
 
     if (!user) {
-      throw new AppError("User does not exists!")
+      throw new AppError("User does not exists!");
     }
 
     const token = uuidV4();
 
-    const expires_date = this.dateProvider.addHours(3)
+    const expires_date = this.dateProvider.addHours(3);
 
     await this.userTokensRepository.create({
       refresh_token: token,
       user_id: user.id,
-      expires_date
-    })
+      expires_date,
+    });
 
-    await this.mailProvider.sendMail(email, "Recuperação de senha", `O link para o reset é ${token}`)
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${token}`,
+    };
+
+    await this.mailProvider.sendMail(
+      email,
+      "Recuperação de senha",
+      variables,
+      templatePath
+    );
   }
 }
 
-export { SendForgotPasswordMailUseCase }
+export { SendForgotPasswordMailUseCase };
